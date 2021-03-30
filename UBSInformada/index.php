360b9434c35123136104a6088333823ed8c257be
wh0ismi <57287249+wh0ismi@users.noreply.github.com>
2021-03-30 19:01:21 -0300

    Configuracoes iniciais de dependencias + inicio do desenvolvimento do Frontend.

---
+++ UBSInformada/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <!-- Importação Icons Materialize e CSS Materialize propriamente dito -->
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link rel="stylesheet" href="materialize/css/materialize.min.css">
+        <title></title>
+    </head>
+    <body>
+        <nav class="light-blue darken-1">
+            <div class ="nav-wrapper container">
+                <div class="left"><img src="imagens/logo.PNG" width=90 height=60></div>
+                <ul class="right">
+                    <li><a href=""><i class ="material-icons left">account_box</i>Cadastro</a></li>
+                    <li><a href=""><i class ="material-icons left">search</i>Consulta</a></li>
+                </ul>
+            </div>
+        </nav>
+        
+
+        <!-- Importação JQuery e JavaScript --> 
+        <script type="text/javascript" src="materialize/js/jquery-3.3.1.min.js"></script>
+        <script type="text/javascript" src="materialize/js/materialize.min.js"></script>
+
+        <!-- Inicialização do JQuery --> 
+        <script type="text/javascript">
+            $(document).ready(function () {
+
+            });
+        </script>
+    </body>
+</html>

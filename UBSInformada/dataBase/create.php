02bdea0e32d2ac69022727ec828b598b9bccdd73
wh0ismi <57287249+wh0ismi@users.noreply.github.com>
2021-04-18 13:02:31 -0300

    Inicio da tela de registro de vacinas.

--- UBSInformada/dataBase/create.php
+++ UBSInformada/dataBase/create.php
@@ -4,31 +4,33 @@ include_once 'conexao.php';
 
 $nomeCompleto = filter_input(INPUT_POST, 'nomeCompleto', FILTER_SANITIZE_SPECIAL_CHARS);
 $cpf = filter_input(INPUT_POST, 'cpf', FILTER_SANITIZE_NUMBER_INT);
+$dataNascimento = filter_input(INPUT_POST, 'dataNascimento', FILTER_SANITIZE_NUMBER_INT);
 $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
 $numeroContato = filter_input(INPUT_POST, 'numeroContato', FILTER_SANITIZE_NUMBER_INT);
 $enderecoCompleto = filter_input(INPUT_POST, 'enderecoCompleto', FILTER_SANITIZE_SPECIAL_CHARS);
 $cep = filter_input(INPUT_POST, 'cep', FILTER_SANITIZE_NUMBER_INT);
 
 
-$querySelect = $link->query("select email from tb_beneficiario");
-$array_emails = [];
+$querySelect = $link->query("select cpf from tb_beneficiario");
+$array_cpfs = [];
 
-while ($emails = $querySelect->fetch_assoc()):
-    $emails_existentes = $emails['email'];
-    array_push($array_emails, $emails_existentes);
+while ($cpfs = $querySelect->fetch_assoc()):
+    $cpfs_existentes = $cpfs['cpf'];
+    array_push($array_cpfs, $cpfs_existentes);
 endwhile;
 
-if (in_array($email, $array_emails)):
-    $_SESSION['msg'] = "<p class='center red-text'>".'Já existe um cadastro com esse e-mail.'."</p>";
+if (in_array($cpf, $array_cpfs)):
+    $_SESSION['msg'] = "<p class='center red-text'>".'Já existe um cadastro com esse CPF.'."</p>";
     header("Location:../");
 else: 
     $queryInsert = $link->query("insert into tb_beneficiario values(default,
-                                                                       '$nomeCompleto',
-                                                                       '$cpf',
-                                                                       '$email',
-                                                                       '$numeroContato',
-                                                                       '$enderecoCompleto',
-                                                                       '$cep')");
+                                                                    '$nomeCompleto',
+                                                                    '$cpf',
+                                                                    '$dataNascimento',
+                                                                    '$email',
+                                                                    '$numeroContato',
+                                                                    '$enderecoCompleto',
+                                                                    '$cep')");
     $affect_rows = mysqli_affected_rows($link);
     
     if($affect_rows > 0):
--- UBSInformada/index.php
+++ UBSInformada/index.php
@@ -1,4 +1,5 @@
 <?php session_start() ?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -9,30 +10,31 @@
         <title></title>
     </head>
     <body>
+
         <nav class="light-blue darken-1">
             <div class ="nav-wrapper container">
                 <div class="left"><img src="imagens/logo.PNG" width=90 height=60></div>
                 <ul class="right">
-                    <li><a href=""><i class ="material-icons left">account_box</i>Cadastro</a></li>
-                    <li><a href=""><i class ="material-icons left">search</i>Consulta</a></li>
+                    <li><a href="index.php"><i class ="material-icons left">account_box</i>Cadastro</a></li>
+                    <li><a href="consultas.php"><i class ="material-icons left">search</i>Consulta</a></li>
                 </ul>
             </div>
         </nav>
-        
+
         <div class ="row container">
             <p>&nbsp;</p>
             <form action="dataBase/create.php" method="post" class="col s12">
-                <fieldset class="formulario">
-                    <legend><img src="imagens/avatarCadastro.png" width="120" height="120" /></legend>
-                    <h5 class="light center">Cadastro de Beneficiário</h5>
-                    
-                    <?php 
-                        if(isset($_SESSION['msg'])):
-                            echo $_SESSION['msg'];
-                            session_unset();
-                        endif;
+                <fieldset class="formulario" style="padding: 80px">
+                    <legend><img src="imagens/avatarBeneficiario.png" width="140" height="140" /></legend>
+                    <h4 class="light center">Cadastro de Beneficiário</h4>
+
+                    <?php
+                    if (isset($_SESSION['msg'])):
+                        echo $_SESSION['msg'];
+                        session_unset();
+                    endif;
                     ?>
-                    
+
                     <!-- label nome completo -->
                     <div class="input-field col s12">
                         <i class="material-icons prefix">person</i>
@@ -40,44 +42,49 @@
                         <label for="nomeCompleto">Nome Completo</label>
                     </div>
                     <!-- label cpf -->
-                    <div class="input-field col s12">
+                    <div class="input-field col s6">
                         <i class="material-icons prefix">subtitles</i>
                         <input type="text" name="cpf" id="cpf" maxlength="11" required>
                         <label for="cpf">CPF</label>
                     </div>
+                    <!-- label dataNascimento -->
+                    <div class="input-field col s6">
+                        <i class="material-icons prefix">date_range</i>
+                        <input type="date" name="dataNascimento" id="dataNascimento" maxlength="10" required>
+                        <label for="dataNascimento">Data de Nascimento</label>
+                    </div>
                     <!-- label email -->
-                    <div class="input-field col s12">
+                    <div class="input-field col s8">
                         <i class="material-icons prefix">email</i>
                         <input type="email" name="email" id="email" maxlength="50" required>
                         <label for="email">E-mail</label>
                     </div>
                     <!-- label numeroContato -->
-                    <div class="input-field col s12">
+                    <div class="input-field col s4">
                         <i class="material-icons prefix">contact_phone</i>
                         <input type="tel" name="numeroContato" id="numeroContato" maxlength="20" required>
                         <label for="numeroContato">Telefone</label>
                     </div>
-                    <!-- label enderecoComplero -->
-                    <div class="input-field col s12">
-                        <i class="material-icons prefix">person_pin_circle</i>
-                        <input type="text" name="enderecoCompleto" id="enderecoCompleto" maxlength="100" required>
-                        <label for="enderecoComplero">Endereço</label>
-                    </div>
                     <!-- label cep -->
-                    <div class="input-field col s12">
+                    <div class="input-field col s4">
                         <i class="material-icons prefix">near_me</i>
                         <input type="text" name="cep" id="cep" maxlength="10" required>
                         <label for="cep">CEP</label>
                     </div>
+                    <!-- label enderecoComplero -->
+                    <div class="input-field col s8">
+                        <i class="material-icons prefix">person_pin_circle</i>
+                        <input type="text" name="enderecoCompleto" id="enderecoCompleto" maxlength="100" required>
+                        <label for="enderecoComplero">Endereço</label>
+                    </div>
                     <!-- botões -->
-                    <div class="input-field col s12">
+                    <div class="center input-field col s12">
                         <input type="submit" value="cadastrar" class="btn light-blue darken-1">
                         <input type="reset" value="limpar" class="btn red darken-1">
                     </div>
                 </fieldset>
             </form>
         </div>
-        
 
         <!-- Importação JQuery e JavaScript --> 
         <script type="text/javascript" src="materialize/js/jquery-3.3.1.min.js"></script>

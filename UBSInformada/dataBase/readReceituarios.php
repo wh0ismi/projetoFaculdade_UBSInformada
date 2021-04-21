987e4f0a9541b8013806c63967766184fcd62fbd
Higor Konig <higor.konig@gmail.com>
2021-04-21 13:45:04 -0300

    Colocando o where nos read e condicional que verifica linhas afetadas

--- UBSInformada/dataBase/readReceituarios.php
+++ UBSInformada/dataBase/readReceituarios.php
@@ -3,17 +3,23 @@ include_once 'conexao.php';
 
 $querySelectReceituario = $link->query("SELECT dataEmissao, idBeneficiario, unidadeSaude, nomeMedico, crm
                                    FROM tb_receituarios 
-                                   INNER JOIN tb_beneficiarios on tb_beneficiarios.id = tb_receituarios.idBeneficiario");
+                                   INNER JOIN tb_beneficiarios on tb_beneficiarios.id = tb_receituarios.idBeneficiario  WHERE idBeneficiario='$id'");
 
-while($registros = $querySelectReceituario->fetch_assoc()):
-    $dataEmissao = $registros['dataEmissao'];
-    $idBeneficiario = $registros['idBeneficiario'];
-    $unidadeSaude = $registros['unidadeSaude'];
-    $nomeMedico = $registros['nomeMedico'];
-    $crm = $registros['crm'];
-    
+if($querySelectReceituario->num_rows > 0) {
+    while($registros = $querySelectReceituario->fetch_assoc()):
+        $dataEmissao = $registros['dataEmissao'];
+        $idBeneficiario = $registros['idBeneficiario'];
+        $unidadeSaude = $registros['unidadeSaude'];
+        $nomeMedico = $registros['nomeMedico'];
+        $crm = $registros['crm'];
+        
+        echo "<tr>";
+        echo "<td>$dataEmissao</td><td>$idBeneficiario</td><td>$unidadeSaude</td><td>$nomeMedico</td><td>$crm</td>"; 
+        echo "</tr>";
+    endwhile;
+}else {
     echo "<tr>";
-    echo "<td>$dataEmissao</td><td>$idBeneficiario</td><td>$unidadeSaude</td><td>$nomeMedico</td><td>$crm</td>"; 
+    echo "<td colspan='5'>Nenhum resultado encontrado!</td>"; 
     echo "</tr>";
-endwhile;
+}
 
